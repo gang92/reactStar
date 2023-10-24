50c5ce599033b76f89f71d0510181f7032f7c49d
윤동현 <hn01291@naver.com>
2023-10-24 19:34:29 +0900

    네이버 지도 1차 업데이트

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -9,6 +9,7 @@ import {RestaurantList} from "./components/RestaurantList";
 import {RestaurantSearchForm} from "./components/RestaurantSearchForm";
 import {Header} from "./components/Header"
 import { ShowRestaurantDetail } from './components/ShowRestaurantDetail';
+import NaverMap from './components/NaverMap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +17,7 @@ root.render(
     <Header />
     <RestaurantSearchForm />
     <RestaurantList />
+    <NaverMap />
   </React.StrictMode>
 );
 
